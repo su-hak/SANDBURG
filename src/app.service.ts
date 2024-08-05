c285d1016d66f71f8a4a47b27f2fd35a54e10404
kimsuhak <uiouij@naver.com>
2024-08-06 00:29:06 +0900

    초기화면 모바일에서 글자 안 보임. 폰트 사이즈 및 위치 조절

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `<a href="/api-docs">swagger ui로 이동</a>`;
+    return `
+    <body style="display: flex; align-items: center; flex-direction: row; justify-content: center;">
+    <a href="/api-docs" style="font-size: 40px; color: black; font-weight: 900;">swagger ui로 이동</a>
+    </body>
+    `;
   }
 }
